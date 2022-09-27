4f3fbc885e711b61a63992fb9b93eeec3eea4e9a
Rikanskey <riko3144@gmail.com>
2022-09-27 23:20:08 +0300

    Init commit

--- pages/hour/index.js
+++ pages/hour/index.js
@@ -3,8 +3,11 @@ const plain = () => null;
 export const getServerSideProps = async ({ res }) => {
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('author', 'i-volobuev');
-    let date = new Date;
-    res.end((date.getHours()).toString())
+    let date = new Date
+    let date_local = date.toLocaleString("ru-Ru", {timeZone: "Europe/Moscow"})
+        .split(':')[0]
+        .split(',')[1]
+    res.end(date_local)
     return { props: {} };
 }
 
